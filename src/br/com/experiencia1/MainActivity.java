985d1ef20de0587c7d8f266492e5d6de22c26b49
Jon <jonjcosta@gmail.com>
2012-11-25 20:20:05 -0200

    25/11 - Mudanças para calculo de menor caminho

--- src/br/com/experiencia1/MainActivity.java
+++ src/br/com/experiencia1/MainActivity.java
@@ -28,28 +28,33 @@ public class MainActivity extends Activity implements LocationListener {
     public void onClick(View v){
     	switch(v.getId()){
     	case R.id.btnInicio:
-    		HashMap params = getCoordenadas() ; //É criado um HashMap que possui os dados de coordenadas GPS do usuário
-    			//Toast.makeText(this, "Teste:", Toast.LENGTH_LONG).show() ;
+    		String placa = null ;
+    		//É criado um HashMap que possui os dados de coordenadas GPS do usuário
+    		HashMap<String, Double> params = getCoordenadas() ; 
 
     		//Cria-se o objeto JSON a partir do HashMap
             JSONObject jsonParams = new JSONObject(params);
             
             //Obtém-se a 'reposta' da WebService, defindo o método a ser acessado e os parâmetros
         	JSONObject resp = HttpClient.SendHttpPost(this.getString(R.string.url_ws), jsonParams);
+        	
         	try {
-				verTaxiProx(resp) ;
+				placa = resp.getString("Placa") ;
 			} catch (JSONException e) {
 				e.printStackTrace();
 			}
+        	
+        	Toast.makeText(this, "Placa: "+placa, Toast.LENGTH_LONG).show() ;
+        	
     		break ;
     	}//Fecha switch
     }//Fecha onClick
     
-    public HashMap getCoordenadas(){
+    public HashMap<String, Double> getCoordenadas(){
     	
     	double lat = 0, lng = 0 ;
     	
-    	HashMap params = new HashMap();
+    	HashMap<String, Double> params = new HashMap<String, Double>();
     	LocationManager LM = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
     	LM.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
         String bestProvider = LM.getBestProvider(new Criteria(), true) ;
@@ -59,7 +64,9 @@ public class MainActivity extends Activity implements LocationListener {
         
         //Obtém-se os valores de latitude e longitude
         lat = local.getLatitude() ;
-        lng = local.getLongitude();
+        lng = local.getLongitude() ;
+        
+        //Log.d("TESTE", "Lat: "+lat+ " Lng: "+log) ;
         
         //Valores são inseridos no HashMap
         params.put("latitude", lat);
@@ -68,58 +75,6 @@ public class MainActivity extends Activity implements LocationListener {
     	return params ;
     	
     }//Fecha getCoordenadas
-    
-    public void verTaxiProx(JSONObject params) throws JSONException{
-    	
-    	try{
-    		
-    		double tax1lng, tax1lat, tax2lng, tax2lat, tax3lng, tax3lat ;
-    		String tax1placa, tax2placa, tax3placa ;
-    		
-    		//Obtém os táxis a partir do objeto JSON
-    		
-    		tax1lng = params.getDouble("tax1lng") ;
-    		tax1lat = params.getDouble("tax1lat") ;
-    		tax1placa = params.getString("tax1placa") ;
-    		
-    		tax2lng = params.getDouble("tax2lng") ;
-    		tax2lat = params.getDouble("tax2lat") ;
-    		tax2placa = params.getString("tax2placa") ;
-    		
-    		tax3lng = params.getDouble("tax3lng") ;
-    		tax3lat = params.getDouble("tax3lat") ;
-    		tax3placa = params.getString("tax3placa") ;
-    		
-        	//São criados objetos Location a partir dos dados de cada táxi
-        	Location loc1 = new Location("Local 1");
-        	Location loc2 = new Location("Local 2") ;
-        	Location loc3 = new Location("Local 3") ;
-        	 
-    		
-        	loc1.setLatitude(tax1lat) ;
-        	loc1.setLongitude(tax1lng) ;
-        	
-        	loc2.setLatitude(tax2lat) ;
-        	loc2.setLongitude(tax2lng) ;
-        	
-        	loc3.setLatitude(tax3lat) ;
-        	loc3.setLongitude(tax3lng) ;
-        	
-        	if(local.distanceTo(loc1) < local.distanceTo(loc2) && local.distanceTo(loc1) < local.distanceTo(loc3)){
-        		Toast.makeText(this, "Placa do Táxi mais próximo: " +tax1placa, Toast.LENGTH_LONG).show() ;
-        	}else if(local.distanceTo(loc2) < local.distanceTo(loc1) && local.distanceTo(loc2) < local.distanceTo(loc3)){
-        		Toast.makeText(this, "Placa do Táxi mais próximo: " +tax2placa, Toast.LENGTH_LONG).show() ;
-        	}else if(local.distanceTo(loc3) < local.distanceTo(loc1) && local.distanceTo(loc3) < local.distanceTo(loc2)){
-        		Toast.makeText(this, "Placa do Táxi mais próximo: " +tax3placa, Toast.LENGTH_LONG).show() ;
-        	}else{
-        		Toast.makeText(this, "Erro", Toast.LENGTH_LONG).show() ;
-        	}
-    	
-    	}catch(JSONException e){
-    		e.printStackTrace() ;
-    	}//Fecha catch
-    	
-    }//Fecha verTaxiProx
     
     /*MÉTODOS DE LOCATION LISTENER*/
 	@Override
